7140b091daaddedd84f6926877292454ef6f3b93
marcelkamp1 <57356700+marcelkamp1@users.noreply.github.com>
2020-01-27 22:37:54 +0100

    Bugfixes

--- src/main/java/com/bankzecure/webapp/repository/CustomerRepository.java
+++ src/main/java/com/bankzecure/webapp/repository/CustomerRepository.java
@@ -55,46 +55,57 @@ public class CustomerRepository {
     return null;
   }
 
-  public Customer update(String identifier, String newEmail, String newPassword) {
+ 	public Customer update(String identifier, String newEmail, String newPassword) {
 
-    Connection connection = null;
-    PreparedStatement statement = null;
-    ResultSet resultSet = null;
-    Customer customer = null;
-    try {
-        // Connection and statement
-        connection = DriverManager.getConnection(
-          DB_URL, DB_USERNAME, DB_PASSWORD
-        );
-        statement = connection.prepareStatement("UPDATE customer SET email = ?, password = ? WHERE identifier = ?");
-        statement.setString(1, newEmail);
-        statement.setString(2, newPassword);
-        statement.setString(3, identifier);
-        statement.executeUpdate();
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		try {
+			// Connection and statement
+			connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
 
-        JdbcUtils.closeStatement(statement);
-        JdbcUtils.closeConnection(connection);
+			// Don't set the password in the update query, if it's not provided
+			if (newPassword != "") {
+				statement = connection.prepareStatement("UPDATE customer SET email = ?, password = ? WHERE identifier = ?");
+				statement.setString(1, newEmail);
+				statement.setString(2, newPassword);
+				statement.setString(3, identifier);
+			} else {
+				statement = connection.prepareStatement("UPDATE customer SET email = ? WHERE identifier = ?");
+				statement.setString(1, newEmail);
+				statement.setString(2, identifier);
+			}
 
-        connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-        statement = connection.prepareStatement("SELECT * FROM customer WHERE identifier = ?");
-        statement.setString(1, identifier);
-        resultSet = statement.executeQuery();
+			if (statement.executeUpdate() != 1) {
+				throw new SQLException("failed to update data");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JdbcUtils.closeStatement(statement);
+			JdbcUtils.closeConnection(connection);
+		}
 
-        if (resultSet.next()) {
-          final int id = resultSet.getInt("id");
-          final String identifierInDb = resultSet.getString("identifier");
-          final String firstName = resultSet.getString("first_name");
-          final String lastName = resultSet.getString("last_name");
-          final String email = resultSet.getString("email");
-          customer = new Customer(id, identifierInDb, firstName, lastName, email);
-        }
-        return customer;
-    } catch (SQLException e) {
-        e.printStackTrace();
-    } finally {
-   	    JdbcUtils.closeStatement(statement);
-   	    JdbcUtils.closeConnection(connection);
-    }
-    return null;
+		try {
+			connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+			statement = connection.prepareStatement("SELECT * FROM customer WHERE identifier = ?");
+			statement.setString(1, identifier);
+			resultSet = statement.executeQuery();
+
+			while (resultSet.next()) {
+				Long id = resultSet.getLong("id");
+				String firstName = resultSet.getString("first_name");
+				String lastName = resultSet.getString("last_name");
+				String email = resultSet.getString("email");
+				return new Customer(id, identifier, firstName, lastName, email);
+			}
+		} catch (final SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JdbcUtils.closeResultSet(resultSet);
+			JdbcUtils.closeStatement(statement);
+			JdbcUtils.closeConnection(connection);
+		}
+		return null;
 }
 }
